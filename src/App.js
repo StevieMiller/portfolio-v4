18455d2bc7a6f28bd8e5c2c4f33035ccd0414d20
Stevie Galaxy <stephanie.miller@carlsbadschools.net>
2023-06-14 11:31:07 -0700

    added landing page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import "./styles/global.css";
 import Header from "./components/Header";
+import Landing from "./pages/Landing";
 
 function App() {
   return (
     <>
       <Header />
+      <Landing />
     </>
   );
 }
---
+++ src/pages/Landing.js
@@ -0,0 +1,11 @@
+export default function Landing() {
+  return (
+    <section className="landing">
+      <h1>Hi, my name is Stevie Miller</h1>
+      <p>
+        I'm a full-stack web developer focused on creating user experiences that
+        are intuitive, efficient, and aesthetically pleasing
+      </p>
+    </section>
+  );
+}
